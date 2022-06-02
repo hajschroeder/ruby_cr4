84c5ed26232e3e2b24e933afd5aaa306f76086b6
Hans Schroeder <hajschroeder@gmail.com>
2022-06-02 14:04:23 -0700

    include code for validation tests. 12/12 tests green

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates :content_body, presence: true
+  validates :rating, presence: true
+  validates_length_of :content_body, maximum: 250
+  validates_length_of :content_body, minimum: 50
+  validates_numericality_of :rating, only_integer: true
+  validates_numericality_of :rating, less_than_or_equal_to: 5
+  validates_numericality_of :rating, greater_than_or_equal_to: 1
 end
