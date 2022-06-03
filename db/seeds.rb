5dc792515b595a041c3ee80a21110789d45b1911
Hans Schroeder <hajschroeder@gmail.com>
2022-06-03 13:54:13 -0700

    include faker, wrote code for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+50.times do |index|
+  Product.create!(name: Faker::Food.dish, cost: Faker::Number.number(digits: 2), country_of_origin: Faker::Address.country)
+end
+
+p "Created #{Product.count} products"
+
+Review.destroy_all
+250.times do |index|
+  Review.create!(author: Faker::Fantasy::Tolkein.character, content_body: Faker::Books::Lovecraft.paragraph_by_chars(characters: 75), rating: Faker::Number.between(from: 1, to: 5), product_id: product.id )
+end
+
+p "Created #{Review.count} reviews"
+
