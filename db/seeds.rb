4e0638633887d70f4d347cfe338130da4c53b99c
Hans Schroeder <hajschroeder@gmail.com>
2022-06-11 12:16:04 -0700

    fixed bug in seeds.rb, database successfully seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,12 @@ Product.destroy_all
 Review.destroy_all 
 
 50.times do |index|
-  Product.create!(name: Faker::Food.dish, cost: Faker::Number.number(digits: 2), country_of_origin: Faker::Address.country,)
+  product = Product.create!(name: Faker::Food.dish, cost: Faker::Number.number(digits: 2), country_of_origin: Faker::Address.country,)
 
 
   5.times do |index|
-    Review.create!(author: Faker::Fantasy::Tolkien.character, content_body: Faker::Books::Lovecraft.paragraph_by_chars(characters: 75), rating: Faker::Number.between(from: 1, to: 5), product_id: rand(2500) )
+    Review.create!(author: Faker::Fantasy::Tolkien.character, content_body: Faker::Books::Lovecraft.paragraph_by_chars(characters: 75), rating: Faker::Number.between(from: 1, to: 5), product_id: product.id )
   end
-
+  p "Created #{Review.count} reviews"
 end
-
-
-
 p "Created #{Product.count} products"
